78a3c8c6cf3a34ce3717ba3a9ba49b6f1aebd0c8
CarlosPerez <carlosmperez2901@gmail.com>
2025-02-22 16:59:26 -0800

    Add Display books functionality

--- index.js
+++ index.js
@@ -12,3 +12,19 @@ function addBookToLibrary(book) {
     library.push(book);
     return;
 }
+
+function displayBooks() {
+    library.forEach(() => {
+        let card = document.createElement("div");
+        let cardTitle = document.createElement("p").classList.add("card-title");
+        let author = document.createElement("p");
+        let pages = document.createElement("p");
+
+        let deleteButton = document.createElement("button");
+        deleteButton.value = "Delete";
+
+        let readButton = document.createElement("button");
+        readButton.value = "Read";
+    })
+
+}
