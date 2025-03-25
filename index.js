f2e05d7cbf86330d5ac4fa3f11c57d0bdc8b825c
CarlosPerez <carlosmperez2901@gmail.com>
2025-03-24 20:49:30 -0700

    Refactor: Change constructor functions for classes to improve scalabilty

--- index.js
+++ index.js
@@ -1,234 +1,291 @@
 const library = [];
-const addButton = document.querySelector("button");
+const addButton = document.querySelector('button');
 
-function Book(title, author, pageNumbers) {
-    this.title = title;
-    this.author = author;
-    this.pages = pageNumbers;
-    this.read = false;
 
-    return this;
+class Book {
+  #title;
+  #author;
+  #pages;
+  #read = false;
+
+  constructor(title, author, pages) {
+    this.#title = title;
+    this.#author = author;
+    this.#pages = pages;
+  }
+
+  get title() { return this.#title; }
+  get author() { return this.#author; }
+  get pages() { return this.#pages; }
+  get isRead() { return this.#read; }
+
+  toggleRead() {
+    if (this.#read) this.#read = false;
+    else this.#read = true;
+  }
+
 }
 
-function addBookToLibrary(book) {
-    library.push(book);
+class BookCardUI {
+  #card;
+  #book;
+  #trashIcon;
+  #readIcon;
+  #doneIcon;
+  #title;
+  #author;
+  #pages;
+  #deleteButton;
+  #readButton;
 
-}
+  constructor(book) {
+    this.#card = document.createElement('div');
+    this.#book = book;
+    this.#createTrasIcon('icons/delete.svg');
+    this.#createReadIcon('icons/read.svg');
+    this.#createDoneIcon('icons/done.svg');
+    this.#createTitleElement();
+    this.#createAuthorElement();
+    this.#createPagesElement();
+    this.#createDeleteButton();
+    this.#createReadButton();
 
-function createBookCardUI(book) {
-    const trashIcon = document.createElement("img");
-    trashIcon.src = "icons/delete.svg";
-    trashIcon.classList.add("icon")
-
-
-    const readIcon = document.createElement("img");
-    readIcon.src = "icons/read.svg";
-    readIcon.textContent = "Mark as read"
-    readIcon.classList.add("icon")
-
-    const doneIcon = document.createElement("img");
-    doneIcon.src = "icons/done.svg";
-    doneIcon.classList.add("icon");
-    doneIcon.classList.add("done-icon");
-    doneIcon.classList.add("hidden");
-
-
-    const card = document.createElement("div");
-    card.classList.add("book-card");
-    card.classList.add("card");
-
-
-    const title = document.createElement("p");
-    title.classList.add("book-title");
-    title.textContent = `${book.title}`
-
-    const author = document.createElement("p");
-    author.textContent = `By: ${book.author} `
-
-    const pages = document.createElement("p");
-    pages.textContent = `Pages: ${book.pages}`
+    this.#card.classList.add('book-card');
+    this.#card.classList.add('card');
 
 
-    const deleteButton = document.createElement("button");
-    deleteButton.textContent = "Delete"
-    deleteButton.classList.add("delete-button")
-    deleteButton.classList.add("clickable")
-    deleteButton.appendChild(trashIcon);
-
-    const readButton = document.createElement("button");
-    readButton.textContent = "Mark as Read";
-    readButton.classList.add('clickable');
-    readButton.classList.add("read-button")
-    if (book.read) {
-        doneIcon.classList.toggle("hidden");
-        card.classList.toggle("read");
-        readButton.textContent = "Mark as Not Read";
-        readIcon.src = "icons/not-read.svg"
+    if (book.isRead) {
+      this.#doneIcon.classList.toggle('hidden');
+      this.#card.classList.toggle('read');
+      this.#readButton.textContent = 'Mark as Not Read';
+      this.#readIcon.src = 'icons/not-read.svg';
     }
-    readButton.appendChild(readIcon);
+    this.#readButton.appendChild(this.#readIcon);
 
 
-    card.appendChild(doneIcon);
-    card.appendChild(title);
-    card.appendChild(author);
-    card.appendChild(pages);
-    card.appendChild(deleteButton);
-    card.appendChild(readButton);
+    this.#card.appendChild(this.#doneIcon);
+    this.#card.appendChild(this.#title);
+    this.#card.appendChild(this.#author);
+    this.#card.appendChild(this.#pages);
+    this.#card.appendChild(this.#deleteButton);
+    this.#card.appendChild(this.#readButton);
 
-    return card;
+    return this.#card;
+
+  }
+
+  #createReadIcon(src) {
+    this.#readIcon = document.createElement('img');
+    this.#readIcon.src = src;
+    this.#readIcon.textContent = 'Mark as read';
+    this.#readIcon.classList.add('icon');
+  }
+
+  #createDoneIcon(src) {
+    this.#doneIcon = document.createElement('img');
+    this.#doneIcon.src = src;
+    this.#doneIcon.classList.add('icon');
+    this.#doneIcon.classList.add('done-icon');
+    this.#doneIcon.classList.add('hidden');
+
+  }
+  #createTitleElement() {
+    this.#title = document.createElement('p');
+    this.#title.classList.add('book-title');
+    this.#title.textContent = `${this.#book.title}`;
+
+  }
+  #createAuthorElement() {
+    this.#author = document.createElement('p');
+    this.#author.textContent = `By: ${this.#book.author} `;
+
+  }
+  #createPagesElement() {
+    this.#pages = document.createElement('p');
+
+    this.#pages.textContent = `Pages: ${this.#book.pages}`;
+  }
+  #createDeleteButton() {
+    this.#deleteButton = document.createElement('button');
+    this.#deleteButton.textContent = 'Delete';
+    this.#deleteButton.classList.add('delete-button');
+    this.#deleteButton.classList.add('clickable');
+    this.#deleteButton.appendChild(this.#trashIcon);
+
+  }
+  #createReadButton() {
+    this.#readButton = document.createElement('button');
+    this.#readButton.textContent = 'Mark as Read';
+    this.#readButton.classList.add('clickable');
+    this.#readButton.classList.add('read-button');
+
+  }
+
+  #createTrasIcon(src) {
+    this.#trashIcon = document.createElement('img');
+    this.#trashIcon.src = src;
+    this.#trashIcon.classList.add('icon');
+  }
 }
 
+class BookForm {
+  #instance;
 
-function displayBooks(library) {
-    let i = 0;
-    const cardsContainer = document.querySelector(".cards-container");
-    cardsContainer.replaceChildren();
+  constructor() {
+    if (this.#instance) return this.#instance;
+    return this.#createBookForm();
 
-    library.forEach(book => {
-        const card = createBookCardUI(book);
-        card.dataset.indexNumber = i;
-        cardsContainer.appendChild(card);
-        i += 1;
-    })
-}
+  }
 
-function createBookForm() {
+  #createBookForm() {
+    this.#instance = document.createElement('form');
+    this.#instance.method = 'POST';
+    this.#instance.classList.add('card');
 
-    const bookForm = document.createElement("form");
-    bookForm.method = "POST";
-    bookForm.classList.add("card")
-
-
-
-    const titleP = document.createElement("p");
-    const titleInput = document.createElement("input");
-    titleInput.id = "title"
+    const titleP = document.createElement('p');
+    const titleInput = document.createElement('input');
+    titleInput.id = 'title';
     titleInput.required = true;
-    const titleLabel = document.createElement("label");
-    titleLabel.textContent = "Title:";
+    const titleLabel = document.createElement('label');
+    titleLabel.textContent = 'Title:';
     titleLabel.htmlFor = titleInput.id;
     titleP.appendChild(titleLabel);
     titleP.appendChild(titleInput);
 
-    const authorP = document.createElement("p");
-    const authorInput = document.createElement("input");
-    authorInput.id = "author";
+    const authorP = document.createElement('p');
+    const authorInput = document.createElement('input');
+    authorInput.id = 'author';
     authorInput.required = true;
-    const authorLabel = document.createElement("label");
-    authorLabel.textContent = "Author:";
+    const authorLabel = document.createElement('label');
+    authorLabel.textContent = 'Author:';
     authorLabel.htmlFor = authorInput;
     authorP.appendChild(authorLabel);
     authorP.appendChild(authorInput);
 
-    const pagesP = document.createElement("p");
-    const pagesInput = document.createElement("input");
-    pagesInput.type = "number";
-    pagesInput.id = "pages-number";
+    const pagesP = document.createElement('p');
+    const pagesInput = document.createElement('input');
+    pagesInput.type = 'number';
+    pagesInput.id = 'pages-number';
     pagesInput.required = true;
-    const pagesLabel = document.createElement("label");
-    pagesLabel.textContent = "Number of pages:";
+    const pagesLabel = document.createElement('label');
+    pagesLabel.textContent = 'Number of pages:';
     pagesLabel.htmlFor = pagesInput.id;
     pagesP.appendChild(pagesLabel);
     pagesP.appendChild(pagesInput);
 
-    const submitButton = document.createElement("button");
-    submitButton.type = "submit";
-    submitButton.textContent = "Submit";
+    const submitButton = document.createElement('button');
+    submitButton.type = 'submit';
+    submitButton.textContent = 'Submit';
     submitButton.classList.add('clickable');
 
-    const cancelIcon = document.createElement("img");
-    cancelIcon.src = "icons/cancel.svg";
+    const cancelIcon = document.createElement('img');
+    cancelIcon.src = 'icons/cancel.svg';
     cancelIcon.classList.add('cancel-button');
     cancelIcon.classList.add('clickable');
 
 
 
-    bookForm.appendChild(titleP);
-    bookForm.appendChild(authorP);
-    bookForm.appendChild(pagesP);
-    bookForm.appendChild(submitButton)
-    bookForm.appendChild(cancelIcon);
+    this.#instance.appendChild(titleP);
+    this.#instance.appendChild(authorP);
+    this.#instance.appendChild(pagesP);
+    this.#instance.appendChild(submitButton);
+    this.#instance.appendChild(cancelIcon);
 
 
-    return bookForm;
+    return this.#instance;
+  }
+
 }
+
+
+function addBookToLibrary(book) {
+  library.push(book);
+
+}
+
+function displayBooks(library) {
+  let i = 0;
+  const cardsContainer = document.querySelector('.cards-container');
+  cardsContainer.replaceChildren();
+
+  library.forEach((book) => {
+    const card = new BookCardUI(book);
+    card.dataset.indexNumber = i;
+    cardsContainer.appendChild(card);
+    i += 1;
+  });
+}
+
 
 function displayFormUi(form) {
-    blurBackgroundElement.classList.add("blur");
-    blurBackgroundElement.appendChild(form);
-    document.body.appendChild(blurBackgroundElement);
+  blurBackgroundElement.classList.add('blur');
+  blurBackgroundElement.appendChild(form);
+  document.body.appendChild(blurBackgroundElement);
 }
 
-const blurBackgroundElement = document.createElement("div");
-const bookForm = createBookForm();
+const blurBackgroundElement = document.createElement('div');
+const bookForm = new BookForm();
 
-Book.prototype.toggleRead = function() {
-    if (this.read) this.read = false;
-    else this.read = true;
-}
-Book.prototype.isRead = function() {
-    return this.read;
-}
 
-addButton.addEventListener("click", () => {
-    displayFormUi(bookForm);
+addButton.addEventListener('click', () => {
+  displayFormUi(bookForm);
 });
 
 
-document.body.addEventListener("submit", event => {
-    const bookTitle = document.querySelector("#title");
-    const bookAuthor = document.querySelector("#author");
-    const pagesNumber = document.querySelector("#pages-number");
+document.body.addEventListener('submit', (event) => {
+  const bookTitle = document.querySelector('#title');
+  const bookAuthor = document.querySelector('#author');
+  const pagesNumber = document.querySelector('#pages-number');
 
-    const book = new Book(bookTitle.value, bookAuthor.value, pagesNumber.value);
-    addBookToLibrary(book);
-    displayBooks(library);
+  const book = new Book(bookTitle.value, bookAuthor.value, pagesNumber.value);
+  addBookToLibrary(book);
+  displayBooks(library);
 
+  bookForm.reset();
+  document.body.removeChild(blurBackgroundElement);
+  event.preventDefault();
+});
+
+
+document.body.addEventListener('click', (event) => {
+  if (event.target.classList.contains('cancel-button') || event.target.className === 'blur') {
     bookForm.reset();
     document.body.removeChild(blurBackgroundElement);
-    event.preventDefault();
-})
+  }
+  else if (event.target.classList.contains('delete-button')) {
+    const elementIndex = event.target.parentNode.dataset.indexNumber;
+    library.splice(elementIndex, 1);
+    displayBooks(library);
+  }
+  else if (event.target.classList.contains('read-button')) {
+    const elementIndex = event.target.parentNode.dataset.indexNumber;
+    const book = library[elementIndex];
+    book.toggleRead();
+    const isRead = book.isRead;
+    const bookCards = document.querySelectorAll('.book-card');
+    const currentBookCard = bookCards[elementIndex];
+    const readButton = currentBookCard.querySelector('.read-button');
 
 
-document.body.addEventListener("click", (event) => {
-    if (event.target.classList.contains("cancel-button") || event.target.className == "blur") {
-        bookForm.reset();
-        document.body.removeChild(blurBackgroundElement);
+    currentBookCard.classList.toggle('read');
+    const readIcon = document.createElement('img');
+    readIcon.classList.add('icon');
+    const doneIcon = currentBookCard.querySelector('.done-icon');
+    doneIcon.classList.toggle('hidden');
+
+    if (isRead) {
+      readIcon.src = 'icons/not-read.svg';
+      readButton.textContent = 'Mark as Not Read';
     }
-    else if (event.target.classList.contains("delete-button")) {
-        const elementIndex = event.target.parentNode.dataset.indexNumber;
-        library.splice(elementIndex, 1);
-        displayBooks(library);
-    }
-    else if (event.target.classList.contains("read-button")) {
-        const elementIndex = event.target.parentNode.dataset.indexNumber;
-        const book = library[elementIndex];
-        book.toggleRead();
-        const isRead = book.isRead();
-        const bookCards = document.querySelectorAll(".book-card");
-        const currentBookCard = bookCards[elementIndex];
-        const readButton = currentBookCard.querySelector(".read-button");
-
-
-        currentBookCard.classList.toggle("read");
-        const readIcon = document.createElement("img");
-        readIcon.classList.add("icon")
-        const doneIcon = currentBookCard.querySelector(".done-icon");
-        doneIcon.classList.toggle("hidden");
-
-        if (isRead) {
-            readIcon.src = "icons/not-read.svg";
-            readButton.textContent = "Mark as Not Read";
-        }
-        else {
-            readIcon.src = "icons/read.svg";
-            readButton.textContent = "Mark as Read";
-
-        }
-        readButton.appendChild(readIcon);
+    else {
+      readIcon.src = 'icons/read.svg';
+      readButton.textContent = 'Mark as Read';
 
     }
+    readButton.appendChild(readIcon);
+
+  }
 
 
-})
+});
 
